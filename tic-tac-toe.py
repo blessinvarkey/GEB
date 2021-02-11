670adbbadf47c86f5c02ee1430e19b8fd8ceb3e3
Blessin Varkey <blessinvarkey@gmail.com>
2021-02-12 00:16:46 +0530

    update file

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -1,24 +1,22 @@
-def insertLetter(letter, pos):
-    pass
 
-def spaceIsFree(pos):
-    pass
 
-def printBoard(board):
-    pass
-
-def isWinner(bo, le):
-    pass
-
-def playerMove():
-    pass
-
-def compMove():
-    pass
-
-def isBoardFull(board):
-    pass
-
+def start():
+    gplay = [1,2,3,4,5,6,7,8,9]
+    print(      '''          |                 |             ''')
+    print('     ', gplay[0],'  |       ', gplay[1],'       |     ', gplay[2])
+    print(      '''          |                 |             ''')
+    print(''' --------   ---------------   ------------  ''')
+    print(      '''          |                 |             ''')
+    print('     ', gplay[3],'  |       ', gplay[4],'       |     ', gplay[5])
+    print(      '''          |                 |             ''')
+    print(''' --------   ---------------   ------------  ''')
+    print(      '''          |                 |             ''')
+    print('     ', gplay[6],'  |       ', gplay[7],'       |     ', gplay[8])
+    print(      '''          |                 |             ''')
 
 def main():
-    
+    start()
+
+
+if __name__ == '__main__':
+    main()
